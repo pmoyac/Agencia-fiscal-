d4e4823742971f608cd6c055905259a54055921e
adrianag4 <adriana.gutierrez235633@potros.itson.edu.mx>
2024-03-22 21:13:33 -0700

    presentacion
    pantallas principal y modulo licencias

---
+++ ProyectoPresentacion/src/main/java/presentacion/ProyectoPresentacion.java
@@ -0,0 +1,12 @@
+package presentacion;
+
+/**
+ *
+ * @author adria
+ */
+public class ProyectoPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
